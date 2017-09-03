27e4acd523078554fcb47c6ed70ca5befdf9f92e
Kyle Richardson <kyle@ims.uni-stuttgart.de>
2017-09-03 17:43:03 +0200

    added a source file that contains various utilities for building data

---
+++ cynet/util.py
@@ -0,0 +1,24 @@
+## utility functions
+import numpy as np
+
+__all__ = [
+    "build_data",
+    "build_demo_data",
+]
+
+def build_data(config):
+    """Build data for running the seq2seq models
+
+    :param config: the global configuration 
+    """
+    pass
+
+def build_demo_data(config):
+    """Build demo data for running the seq2seq modela
+
+    :param config: the global configuration 
+    """
+    pass 
+    
+
+    
